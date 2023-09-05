69e296e09868a174b1a5844db307ed4a41496a25
Gustavo Italo <gustavolimamdes@gmail.com>
2023-09-05 17:39:25 -0300

    aprendizado de hoje.

---
+++ aprendizados/detalhesSintax.c
@@ -0,0 +1,8 @@
+#include <stdio.h> //o include serve para importar bibliotecas.
+
+int main() // o computador executa tudo que estiver na função main.
+{
+    printf("hello world!\n"); //função basica para printar algo no termainal, o "\n" serve para pular uma linha. 
+
+    return 0; //ainda não sei exatamente para que serve.
+}
