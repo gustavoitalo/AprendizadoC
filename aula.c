a2010f4ac07d34238303efb7b1c3acb852ee3a26
Gustavo <gustavolimamdes@gmail.com>
2023-10-11 10:36:02 -0300

    velocidadeCarro

--- aula.c
+++ aula.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
-int main()
-{
+int main(){
 
-    return 0;
+  int carro;
+
+  printf("qual a velocidade do carro? (km/h)");
+  scanf("%d", &carro);
+
+  if (carro > 80) {
+    carro *= 5;
+    printf("você foi multado em cerca de %d reais", carro);
+  }else {
+    printf("você não vai ser multado!");
+  }
+
+  return 0;
 }
 
 
