654b6f4843764b92ae825f915dbcbf1fff59b101
Gustavo <gustavolimamdes@gmail.com>
2023-10-11 14:43:14 -0300

    loopsDesejados

--- aula.c
+++ aula.c
@@ -2,9 +2,17 @@
 
 int main(){
 
-  int numero;
+  int i = 1;
+  int num;
 
+  printf("quantas vezes?");
+  scanf("%d", &num);
 
+while (i <= num) {
+  printf("%d\n", i);
+  i++;
+}
+printf("Acabou!");
 
   return 0;
 }
