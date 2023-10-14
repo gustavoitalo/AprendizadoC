bd9c188368e66b43d5175fa59bc61eb0e76e9087
Gustavo <gustavolimamdes@gmail.com>
2023-10-14 11:11:03 -0300

    quase terminando o curso

--- aula.c
+++ aula.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
+  
+int media(int a, int b){
+  return (a+b)/2;
+}
 
-  int numero(int a) {
-    printf(" %d ", a);
-    return a;
+  int situacao(int a){
+    if (a <  3) {
+      printf("você esta reprovado! ");
+    }else if (a >= 3 && a <= 5 ){
+      printf("você esta de recuperação! ");
+    }else {
+      printf("você esta Aprovado! ");
+    }
+    return 0;
   }
 
 int main() {
 
-  numero(8);
-
+  situacao(media(3,3));
 
   return 0;
 }
